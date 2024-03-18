1b91696c662f5c19f0a2b0bf2a58b3692ee9925f
GHeeJeon <jhjeon_99@naver.com>
2024-03-18 18:43:28 +0900

    [dev] feat: 생일상 차렸다
    야미~

--- src/components/BirthdayPage.tsx
+++ src/components/BirthdayPage.tsx
@@ -1,10 +1,12 @@
 import AudioPlayer from './AudioPlayer';
+import BirthdayCake from "./BirthdayCake";
 
 function BirthdayPage() {
     return (
         <div className="text-center">
             <h1 className="text-3xl font-bold">🎉 생일축하해요! 🎉</h1>
             <AudioPlayer />
+            <BirthdayCake/>
         </div>
     );
 }
