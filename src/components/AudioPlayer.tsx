d19dad2d8cb8bab56268656e467952b3c0dfed57
GHeeJeon <jhjeon_99@naver.com>
2024-03-20 00:37:07 +0900

    [dev] fix: 오디오 영역 배치 변경

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -39,14 +39,7 @@ const AudioPlayer = () => {
 
     return (
         <div className="flex flex-col justify-center items-center gap-4">
-            <p className="text-sm font-pyeongchang-light">
-                {hasPlayed ? <><br/>헤헤 🥰</> : (
-                    <>
-                        아이폰으로 하나하나 편집해서 브금 만들었는데~~
-                        <br/>아나따가 들어줬음 좋게따~~ 🥺
-                    </>
-                )}
-            </p>
+
             <div className="flex items-center gap-4">
                 <audio ref={audioRef} src="/감자튀김%20옴뇸뇸.m4a" loop muted={isMuted}></audio>
 
@@ -59,8 +52,15 @@ const AudioPlayer = () => {
                 <button onClick={toggleMute}>
                     {isMuted ? <FaVolumeMute/> : <FaVolumeUp/>}
                 </button>
-
             </div>
+            <p className="text-sm font-pyeongchang-light">
+                {hasPlayed ? <><br/>헤헤 🥰</> : (
+                    <>
+                        아이폰으로 하나하나 편집해서 브금 만들었는데~~
+                        <br/>아나따가 들어줬음 좋게따~~ 🥺
+                    </>
+                )}
+            </p>
         </div>
     );
 };
